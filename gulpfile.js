34d58f242e4d0cf47af08cf48a2c544345b178b7
Ivan Lysukhin <ilysukhin@mail.ru>
2020-12-08 12:17:29 +0300

    Добавил оптимизацию графики

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
 const sync = require("browser-sync").create();
 
 // Styles
@@ -19,11 +23,47 @@ const styles = () => {
       csso()
     ]))
     .pipe(sourcemap.write("."))
+    .pipe(rename("style.min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Images
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = images;
+
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 80}))
+    .pipe(gulp.dest("build/img/webp"))
+}
+
+exports.createWebp = createWebp;
+
+// SVG Sprite
+
+const sprite = () => {
+ return gulp.src("source/img/icons/*.svg")
+   .pipe(svgstore())
+   .pipe(rename("svg-sprite.svg"))
+   .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
 
 // Server
 
